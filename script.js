58cef4a1b150dbd114db3b38cfcb423c2fbe0c1c
Valiaaa <valialcc@gmail.com>
2022-10-23 13:22:18 -0400

    update adaptation

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ $(document).ready(function(){
     };
   });
 
+  if ($(window).width() < 640) {
+    $("#caughtmiddle").html("<img src='archive/caught_in_the_middle/final.png' class='pic pica'>")
+  }
+
+  if ($(window).width() < 640) {
+    $("#caughtmiddle2").html("<img src='final.png' class='pic pica'>")
+  }
+
 });
