3fc013a97a507fe81a7a316f5368877801458484
Valiaaa <valialcc@gmail.com>
2023-01-13 20:41:31 -0500

    update js

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('#posterc').change(function(){
+      if ($(this).is(':checked')) {
+          
+      };
+    });
+  
+    // if ($(window).width() < 640) {
+    //   $("#caughtmiddle").html("<img src='archive/caught_in_the_middle/final.png' class='pic pica'>")
+    //   $("#caughtmiddle2").html("<img src='final.png' class='pic pica'>")
+    //   $("#altoona").html("<img src='archive/altoona/altoona cover.png' class='pic pica'>")
+    //   $("#midnightblues").html("<img src='archive/midnight_blues_u/cover.png' class='pic pica'>")
+    //   $("#micronova").html("<img src='archive/micro_nova/1.png' class='pic pica'>")
+    // }
+  
+});
--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
 
-  $('#posterc').change(function(){
-    if ($(this).is(':checked')) {
-        
-    };
-  });
-
-  // if ($(window).width() < 640) {
-  //   $("#caughtmiddle").html("<img src='archive/caught_in_the_middle/final.png' class='pic pica'>")
-  //   $("#caughtmiddle2").html("<img src='final.png' class='pic pica'>")
-  //   $("#altoona").html("<img src='archive/altoona/altoona cover.png' class='pic pica'>")
-  //   $("#midnightblues").html("<img src='archive/midnight_blues_u/cover.png' class='pic pica'>")
-  //   $("#micronova").html("<img src='archive/micro_nova/1.png' class='pic pica'>")
-  // }
+  $('.navigation').html('<h1><a href="../../index.html">Valia Liu</a></h1><hr><h1 class="category disable"><input type="checkbox" id="posterc">&nbsp Poster<br><input type="checkbox" id="editorialc">&nbsp Editorial<br><input type="checkbox" id="webc">&nbsp Website<br><input type="checkbox" id="modelingc">&nbsp Modeling<br><input type="checkbox" id="brandingc">&nbsp Branding<br><input type="checkbox" id="videoc">&nbsp Video<br><input type="checkbox" id="otherc">&nbsp Others<br></h1><hr><h1><a href="../../about.html">About</a></h1><h1><a href="mailto:valialiuwork@gmail.com" target="_blank">Email↗</a></h1>')
 
 });
