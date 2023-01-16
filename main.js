19c73b9c8c104673a48beefe47db758ca469f85b
Valiaaa <valialcc@gmail.com>
2023-01-16 13:51:40 -0500

    update filter

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
 
-    $('#posterc').change(function(){
-      if ($(this).is(':checked')) {
-          
-      };
+// filter checked or not
+  $('.category').change(function(){
+    if ($('input[type="checkbox"]').is(':checked')) {
+      $('.main').hide();
+      $('.filter').show();
+    } else {
+      $('.main').show();
+      $('.filter').hide();
+    };
+
+    $('.filter h2').each(function(){
+      var checkt = 0;
+      var theclass = $(this).attr("class").split(" ");
+      const myArray = theclass;
+      var theclass1 = myArray[0];
+      var theclass2 = myArray[1];
+      if ($("#" + theclass1 + "c").is(':checked') || $("#" + theclass2 + "c").is(':checked')){
+        checkt = checkt +1;
+      } else {
+        checkt = checkt;
+      }
+      if (checkt >=1){
+        $(this).css("display","inline-block");
+      } else {
+        $(this).hide();
+      }
     });
-  
-    // if ($(window).width() < 640) {
-    //   $("#caughtmiddle").html("<img src='archive/caught_in_the_middle/final.png' class='pic pica'>")
-    //   $("#caughtmiddle2").html("<img src='final.png' class='pic pica'>")
-    //   $("#altoona").html("<img src='archive/altoona/altoona cover.png' class='pic pica'>")
-    //   $("#midnightblues").html("<img src='archive/midnight_blues_u/cover.png' class='pic pica'>")
-    //   $("#micronova").html("<img src='archive/micro_nova/1.png' class='pic pica'>")
-    // }
+  });
+
   
 });
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('.navigation').html('<h1><a href="../../index.html">Valia Liu</a></h1><hr><h1 class="category disable"><input type="checkbox" id="posterc">&nbsp Poster<br><input type="checkbox" id="editorialc">&nbsp Editorial<br><input type="checkbox" id="webc">&nbsp Website<br><input type="checkbox" id="modelingc">&nbsp Modeling<br><input type="checkbox" id="brandingc">&nbsp Branding<br><input type="checkbox" id="videoc">&nbsp Video<br><input type="checkbox" id="otherc">&nbsp Others<br></h1><hr><h1><a href="../../about.html">About</a></h1><h1><a href="mailto:valialiuwork@gmail.com" target="_blank">Email↗</a></h1>')
+  $('.navigation').html('<h1><a href="../../index.html">Valia Liu</a></h1><hr><h1 class="category disable"><input type="checkbox" id="albumc">&nbsp Album<br><input type="checkbox" id="posterc">&nbsp Poster<br><input type="checkbox" id="videoc">&nbsp Video<br><input type="checkbox" id="editorialc">&nbsp Editorial<br><input type="checkbox" id="modelingc">&nbsp Modeling<br><input type="checkbox" id="brandingc">&nbsp Branding<br></h1><hr><h1><a href="../../about.html">About</a></h1><h1><a href="mailto:valialiuwork@gmail.com" target="_blank">Email↗</a></h1>')
 
   $("img").css("margin-bottom","1.2%");
   $("video").css("margin-bottom","1.2%");
