54d5473fdd346157080880674870600b97852168
Valiaaa <valialcc@gmail.com>
2023-01-16 15:21:24 -0500

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('.navigation').html('<h1><a href="../../index.html">Valia Liu</a></h1><hr><h1 class="category disable"><input type="checkbox" id="albumc">&nbsp Album<br><input type="checkbox" id="posterc">&nbsp Poster<br><input type="checkbox" id="videoc">&nbsp Video<br><input type="checkbox" id="editorialc">&nbsp Editorial<br><input type="checkbox" id="modelingc">&nbsp Modeling<br><input type="checkbox" id="brandingc">&nbsp Branding<br></h1><hr><h1><a href="../../about.html">About</a></h1><h1><a href="mailto:valialiuwork@gmail.com" target="_blank">Email↗</a></h1>')
+  $('.navigation').html('<h1><a href="../../index.html">Valia Liu</a></h1><hr><h1 class="category disable"><input type="checkbox" id="albumc"><span id="album">&nbsp Album</span><input type="checkbox" id="posterc"><span id="poster">&nbsp Poster</span><input type="checkbox" id="videoc"><span id="video">&nbsp Video</span><input type="checkbox" id="editorialc"><span id="editorial">&nbsp Editorial</span><input type="checkbox" id="modelingc"><span id="modeling">&nbsp Modeling</span><input type="checkbox" id="brandingc"><span id="branding">&nbsp Branding</span></h1><hr><h1><a href="../../about.html">About</a></h1><h1><a href="mailto:valialiuwork@gmail.com" target="_blank">Email↗</a></h1>')
 
   $("img").css("margin-bottom","1.2%");
   $("video").css("margin-bottom","1.2%");
